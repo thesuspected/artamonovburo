ad2a1aabee2ab8c23123de960e93172b1a3f54a5
zuspect <artemmishenko@mail.ru>
2025-02-11 16:35:22 +0400

    add yandex-metrika

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -58,6 +58,22 @@ export default defineNuxtConfig({
                 },
             },
         ],
+        [
+            "nuxt-yandex-metrika",
+            {
+                id: "99875853",
+                // debug: process.env.NODE_ENV !== "production",
+                // delay: 0,
+                // cdn: false,
+                // verification: null, // Verification in Yandex Webmaster
+                options: {
+                    clickmap: true,
+                    trackLinks: true,
+                    accurateTrackBounce: true,
+                    webvisor: true,
+                },
+            },
+        ],
         "@nuxtjs/tailwindcss",
     ],
     vite: {
